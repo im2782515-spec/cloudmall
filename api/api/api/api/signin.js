5dedae56f30d8a4e790b146bc1c17d1ecef26494
im2782515-spec <im2782515@gmail.com>
2026-02-25 03:43:26 +0200

    Update signin.js
    Signed-off-by: im2782515-spec <im2782515@gmail.com>

--- api/api/api/api/signin.js
+++ api/api/api/api/signin.js
@@ -1,5 +1,14 @@
-export default async function handler(req,res){
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.post("/", async (req,res)=>{
   const authResult = req.body.authResult;
-  const me = await axios.get(`https://api.minepi.com/v2/me`, { headers:{Authorization:`Bearer ${authResult.user.accessToken}`} });
-  res.status(200).json({message:"User signed in", me});
-}
+  const me = await axios.get(
+    https://api.minepi.com/v2/me,
+    { headers:{Authorization:Bearer ${authResult.user.accessToken}} }
+  );
+  res.json({message:"User signed in", me: me.data});
+});
+
+module.exports = router;
