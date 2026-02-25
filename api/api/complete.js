5dfef5425b22803fe2ab496652181adda709a4e2
im2782515-spec <im2782515@gmail.com>
2026-02-25 03:42:09 +0200

    Update complete.js
    Signed-off-by: im2782515-spec <im2782515@gmail.com>

--- api/api/complete.js
+++ api/api/complete.js
@@ -1,5 +1,15 @@
-export default async function handler(req,res){
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.post("/", async (req,res)=>{
   const {paymentId, txid} = req.body;
-  await axios.post(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {txid}, { headers:{Authorization:`Key ${process.env.PI_API_KEY}`} });
-  res.status(200).json({message:`Completed ${paymentId}`});
-}
+  await axios.post(
+    https://api.minepi.com/v2/payments/${paymentId}/complete,
+    {txid},
+    { headers:{Authorization:Key ${process.env.PI_API_KEY}} }
+  );
+  res.json({message:Completed ${paymentId}});
+});
+
+module.exports = router;
