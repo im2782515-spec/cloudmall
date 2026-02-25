3619e4f595ca752ff76fbe7f0ab1ebb1e93b3d1b
im2782515-spec <im2782515@gmail.com>
2026-02-26 01:14:39 +0200

    Update server.js
    Signed-off-by: im2782515-spec <im2782515@gmail.com>

--- server.js
+++ server.js
@@ -8,5 +8,7 @@ app.post("/api/approve", require("./api/approve"));
 app.post("/api/complete", require("./api/complete"));
 app.post("/api/signin", require("./api/signin"));
 app.post("/api/incomplete", require("./api/incomplete"));
+const path = require("path");
 
+app.use(express.static(path.join(__dirname,"frontend")));
 app.listen(3000, () => console.log("Server running on port 3000"));
