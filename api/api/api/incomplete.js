646b61586e1cd2ece20bbb370c0d2fba01175ecd
im2782515-spec <im2782515@gmail.com>
2026-02-25 03:44:24 +0200

    Update incomplete.js
    Signed-off-by: im2782515-spec <im2782515@gmail.com>

--- api/api/api/incomplete.js
+++ api/api/api/incomplete.js
@@ -1,6 +1,17 @@
-export default async function handler(req,res){
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.post("/", async (req,res)=>{
   const payment = req.body.payment;
-  // تحقق من الدفع على البلوكتشين إذا مطلوب
-  await axios.post(`/v2/payments/${payment.identifier}/complete`, {txid: payment.transaction.txid}, { headers:{Authorization:`Key ${process.env.PI_API_KEY}`} });
-  res.status(200).json({message:`Handled incomplete ${payment.identifier}`});
-}
+
+  await axios.post(
+    https://api.minepi.com/v2/payments/${payment.identifier}/complete,
+    {txid: payment.transaction.txid},
+    { headers:{Authorization:Key ${process.env.PI_API_KEY}} }
+  );
+
+  res.json({message:Handled incomplete ${payment.identifier}});
+});
+
+module.exports = router;
