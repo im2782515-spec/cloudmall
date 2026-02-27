3c200345bc33d4dbbd9ce2dba084d6a5db658dad
im2782515-spec <im2782515@gmail.com>
2026-02-27 23:18:11 +0200

    Update script.js
    Signed-off-by: im2782515-spec <im2782515@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,50 +1,24 @@
-Pi.init({ version: "2.0" });
+Pi.init({version:"2.0",sandbox:true});
 
-async function login() {
-  try {
-    const auth = await Pi.authenticate(["username","payments"]);
-    
-    await fetch("/api/signin", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(auth)
-    });
-
-    alert("login success");
-  } catch (e) {
-    console.log(e);
-  }
+async function login(){
+ const auth=await Pi.authenticate(["username","payments"]);
+ await fetch("/api/signin",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(auth)});
 }
 
-async function pay() {
-  Pi.createPayment(
-    { amount: 1, memo: "dictionary payment", metadata: {} },
-    {
-      onReadyForServerApproval: async paymentId => {
-        await fetch("/api/approve", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ paymentId })
-        });
-      },
-
-      onReadyForServerCompletion: async paymentId => {
-        await fetch("/api/complete", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ paymentId })
-        });
-      },
-
-      onCancel: paymentId => {
-        fetch("/api/incomplete", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ paymentId })
-        });
-      },
-
-      onError: error => console.log(error)
-    }
-  );
+function pay(){
+ Pi.createPayment(
+  {amount:1,memo:"dictionary",metadata:{}},
+  {
+   onReadyForServerApproval:id=>{
+    fetch("/api/approve",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({id})});
+   },
+   onReadyForServerCompletion:(id,tx)=>{
+    fetch("/api/complete",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({id,tx})});
+   },
+   onCancel:id=>{
+    fetch("/api/incomplete",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({id})});
+   },
+   onError:e=>console.log(e)
+  }
+ );
 }
