3faa38d6151a8b0d19bc8e6b0088061d1a262d5a
im2782515-spec <im2782515@gmail.com>
2026-03-12 03:48:12 +0200

    Update approve.js
    Signed-off-by: im2782515-spec <im2782515@gmail.com>

--- api/approve.js
+++ api/approve.js
@@ -1,4 +1,19 @@
-module.exports = (req,res)=>{
- console.log("approve",req.body);
- res.json({ok:true});
-};
+import axios from "axios";
+
+export default async function handler(req, res) {
+
+  const paymentId = req.body.paymentId;
+
+  const response = await axios.post(
+    "https://api.minepi.com/v2/payments/" + paymentId + "/approve",
+    {},
+    {
+      headers: {
+        Authorization: "Key " + process.env.PI_API_KEY
+      }
+    }
+  );
+
+  res.status(200).json(response.data);
+
+}
